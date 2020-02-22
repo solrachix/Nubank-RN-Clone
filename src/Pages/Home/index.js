539f7890a9ad683a58a3cc957cd386fbfd9b7fb3
Carlos Miguel <carlos.miguel.17@gmail.com>
2020-02-22 11:25:23 -0300

    updating

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,33 +1,11 @@
 import * as React from 'react';
 import { View } from 'react-native';
 
-import TypeWriter from 'react-native-typewriter'
-
-import { ConsoleContainer, Text } from './style';
+// import { ConsoleContainer, Text } from './style';
 
 export default function Home() {
-  const frases = [
-    'Welcome to the react native world.'
-  ];
-
-  function renderText(text){
-    const style = {
-      textAlign: "center"
-    }
-    const onEnd = () => {
-    }
-    return (
-      <TypeWriter style={style} key={text} onTypingEnd={onEnd} minDelay={100} typing={1}>
-        <Text>{text}</Text>
-      </TypeWriter>
-    );
-  }
 
   return (
-    <ConsoleContainer>
-        {frases.map(frase => (
-          renderText(frase)
-        ))}
-    </ConsoleContainer>
+    <View/>
   );
 }
